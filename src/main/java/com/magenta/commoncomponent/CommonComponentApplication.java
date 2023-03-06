730b2893f19b4429e1b9fb1726ec34447ed942d1
Luis Porte <lporte@intelix.biz>
2023-03-06 11:55:34 -0400

    Initial commit

---
+++ src/main/java/com/magenta/commoncomponent/CommonComponentApplication.java
@@ -0,0 +1,13 @@
+package com.magenta.commoncomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonComponentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magenta/commoncomponent/CommonComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magenta.commoncomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
